6328883f1f5b37c0b45be341f86ebec9262d5b59
Jitin Luthra <jitinl@gmail.com>
2015-06-29 03:50:20 +0530

    attempt to generate source maps

--- main.js
+++ main.js
@@ -7,23 +7,71 @@ var path = require('path')
 
 var inFile = argv.compile
 var outDir = argv.output || '.'
+var map = argv.map
+var outScripts
+var outMap
 
+//abort if inFile is not specified
 if (inFile == null) {
     console.error('Error: no input file specified.')
     process.exit(0) //exiting with 0 to make this program work with CodeKit
 }
 
+//Read the merged script from CodeKit
+var inScripts = " " + fs.readFileSync(inFile, {
+        "encoding": "UTF-8"
+    }) //space added to ensure first script below is always a JSX/ES6 script (for the edge case where the first script begins with a backtick)
 
-var inScripts=" "+fs.readFileSync(inFile,{"encoding":"UTF-8"}) //space added to ensure first script below is always a JSX/ES6 script (for the edge case where the first script begins with a backtick)
-var scripts=inScripts.split('`')
-//odd scripts are plain JS scripts and even scripts are JSX/ES6 scripts to be compiled by Babel (counting begins from 0)
-for (var i=0;i<scripts.length;i++){
-    if(i%2==0){
-        var result=babel.transform(scripts[i],{blacklist:["strict"]})
-        scripts[i]=result.code
+
+
+if (map == true) {
+    //combine all scripts and compile the result using Babel
+    var scripts = inScripts.replace("`", ";")
+    var result = babel.transform(scripts, {
+        blacklist: ["strict"],
+        sourceMaps: true,
+        sourceFileName: path.basename(inFile),
+        sourceMapTarget: path.basename(inFile,'.coffee')+'.js'
+
+    })
+    outScripts = result.code+"\n\n//# sourceMappingURL="+path.basename(inFile,'.coffee')+'.js.map\n'
+    outMap = result.map
+} else {
+    //Only the .coffee scripts will be combined by Babel and plain JS scripts with the .js extension will be left alone.
+    //Split the scripts by the backtick character (CodeKit encloses plain JS scripts within backticks)
+    var scripts = inScripts.split('`')
+    for (var i = 0; i < scripts.length; i++) {
+        //odd-numbered scripts are plain JS scripts to be left alone; even-numbered scripts are JSX/ES6 scripts to be compiled by Babel (counting begins from 0)
+        if (i % 2 == 0) {
+            var result = babel.transform(scripts[i], {
+                blacklist: ["strict"]
+            })
+            scripts[i] = result.code
+        }
     }
+    outScripts = scripts.join(';')
 }
 
-var outScripts=scripts.join(';')
-var scriptPath=outDir + "/" + path.basename(inFile, '.coffee') + '.js'
-fs.writeFileSync(scriptPath,outScripts)
+
+
+//write the compiled and merged script
+var scriptPath = outDir + "/" + path.basename(inFile, '.coffee') + '.js'
+fs.writeFile(scriptPath, outScripts, function(err) {
+        if (err) {
+            console.error("Error while trying to write compiled script:", err)
+            process.exit(1)
+        }
+    })
+
+//write the source map
+if (map == true) {
+    var mapPath = scriptPath + '.map'
+    var finalMap={"version":outMap.version, "file":outMap.file, "sourceRoot": outMap.sourceRoot, "sources": outMap.sources, "names":[], "mappings":outMap.mappings  }
+    fs.writeFile(mapPath, JSON.stringify(finalMap, null, 2), function(err) {
+        if (err){
+            console.error("Error while trying to write source map: ", err)
+            process.exit(1)
+        }
+        
+    })
+}
--- test/test.js
+++ test/test.js
@@ -1,68 +1,90 @@
 require('shelljs/global')
-var fs=require("fs")
-var expect=require('chai').expect;
+var fs = require("fs")
+var expect = require('chai').expect;
+var tempDir = tempdir() + 'tmpCodekitJsxHack/'
 
-describe("Codekit JSX hack tests",function(){
-	this.timeout(5000);
-	it("should exit with code 0 when called without any arguments",function(){
-		var output=exec('node main.js',{silent:true}).code
-		expect(output).to.equal(0)
-	})
-	it("Should compile a JSX file with a .coffee extension to a .js file",function(){
-		var tempDir=tempdir()+'tmpCodekitJsxHack/'
+describe("Codekit JSX hack tests", function() {
+    this.timeout(35000);
+    it("should exit with code 0 when called without any arguments", function() {
+        var output = exec('node main.js', {
+            silent: true
+        }).code
+        expect(output).to.equal(0)
+    })
+    it("Should compile a JSX file with a .coffee extension to a .js file", function() {
+        //cleanup temp directory
+        rm('-rf', tempDir)
+        mkdir('-p', tempDir)
 
-		//cleanup temp directory
-		rm('-rf',tempDir)
-		mkdir('-p',tempDir)
+        //write the test JSX script to the temp dir
+        var script = "var jsxVariable=<div/>"
+        script.to(tempDir + 'tmpscript.coffee')
 
-		//write the test JSX script to the temp dir
-		var script="var jsxVariable=<div/>"
-		script.to(tempDir+'tmpscript.coffee')
+        //Check that the script is processed successfuly
+        var output = exec('node main.js --output ' + tempDir + ' --compile ' + tempDir + 'tmpscript.coffee', {
+            silent: true
+        }).code
+        expect(output).to.be.equal(0)
 
-		//check that exit code==0 on executing script on test file
-		var output=exec('node main.js --output '+tempDir+' --compile '+tempDir+'tmpscript.coffee', {silent:true}).code
-		expect(output).to.equal(0)
+        //check that the compiled file tmpscript.js exists
+        var exists = ls(tempDir + 'tmpscript.js')
+        expect(exists.length).to.be.equal(1)
 
-		//check that the compiled file tmpscript.js exists
-		var exists=ls(tempDir+'tmpscript.js')
-		expect(exists.length).to.be.equal(1)
+        //check that the compiled file tmpscript.js is not empty
+        var file = cat(tempDir + "tmpscript.js")
+        expect(file.length).to.be.greaterThan(0)
 
-		//check that the compiled file tmpscript.js is not empty
-		var file=cat(tempDir+"tmpscript.js")
-		expect(file.length).to.be.greaterThan(0)
+        //cleanup temp dir
+        rm('-rf', tempDir)
+    })
+    it("Should correctly compile a merged js-jsx file from CodeKit", function() {
+        //cleanup temp directory
+        rm('-rf', tempDir)
+        mkdir('-p', tempDir)
 
-		//cleanup temp dir
-		rm('-rf',tempDir) 
-	})
-	it("Should correctly compile a merged js-jsx file from CodeKit",function(){
-		var tempDir=tempdir()+'tmpCodekitJsxHack/'
+        //write a test JS-(JSX/ES6) merged script to the temp dir - 1st and 3rd scripts are scripts using new ES6 features while 2nd and 4th are regular javascript
+        //in practice, CodeKit will generate this script when merging Javascript and CoffeeScript files.
+        var mergedScript = "[exports.x,,]=[1,2,4] `exports.v='hello'` let me='go'; exports.y=me`exports.w='bye'`"
+        mergedScript.to(tempDir + 'tmpscript.coffee')
 
-		//cleanup temp directory
-		rm('-rf',tempDir)
-		mkdir('-p',tempDir)
+        //Check that the script is processed successfuly
+        var output = exec('node main.js --output ' + tempDir + ' --compile ' + tempDir + 'tmpscript.coffee').code
+        expect(output).to.be.equal(0)
 
-		//write a test JS-(JSX/ES6) merged script to the temp dir - 1st and 3rd scripts are scripts using new ES6 features while 2nd and 4th are regular javascript
-		var mergedScript="[exports.x,,]=[1,2,4] `exports.v='hello'` let me='go'; exports.y=me`exports.w='bye'`"
+        //check that the compiled file exists
+        var exists = ls(tempDir + 'tmpscript.js')
+        expect(exists.length).to.be.equal(1)
 
-		var script=mergedScript
-		script.to(tempDir+'tmpscript.coffee')
+        //run the compiled file to see it does not throw any errors
+        var testScript = require(tempDir + "tmpscript.js")
+        expect(testScript.x).to.exist
+        expect(testScript.y).to.exist
+        expect(testScript.v).to.exist
+        expect(testScript.w).to.exist
 
-		//check that exit code==0 on executing script on test file
-		var output=exec('node main.js --output '+tempDir+' --compile '+tempDir+'tmpscript.coffee').code
-		expect(output).to.equal(0)
+        //cleanup temp dir
+        rm('-rf', tempDir)
+    })
+    it("Should generate source maps", function() {
+        //cleanup temp directory
+        rm('-rf', tempDir)
+        mkdir('-p', tempDir)
 
-		//check that the compiled file exists
-		var exists=ls(tempDir+'tmpscript.js')
-		expect(exists.length).to.be.equal(1)
+        //Read the React.js file as a test script
+        var script = fs.readFileSync("test/resources/react-0.13.3.js", {
+            "encoding": "UTF-8"
+        })
+        script.to(tempDir + 'tmpscript.coffee')
 
-		//run the compiled file to see it does not throw any errors
-		var testScript=require(tempDir+"tmpscript.js")
-		expect(testScript.x).to.exist
-		expect(testScript.y).to.exist
-		expect(testScript.v).to.exist
-		expect(testScript.w).to.exist
+        //Check that the script is processed successfuly
+        var output=exec('node main.js --map --output ' + tempDir + ' --compile ' + tempDir + 'tmpscript.coffee').code
+        expect(output).to.be.equal(0)
 
-		//cleanup temp dir
-		rm('-rf',tempDir)
-	})
+        //Check that the source map was created
+		var file = cat(tempDir + "tmpscript.js.map")
+        expect(file.length).to.be.greaterThan(10)
+
+        //cleanup temp dir
+        rm('-rf', tempDir)
+    })
 })
